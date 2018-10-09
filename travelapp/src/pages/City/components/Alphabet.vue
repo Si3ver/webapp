e4f898d24736abb52f5f6a43fb2247d0e5bf9c94
Si3ver <zhou_weilin@outlook.com>
2018-10-09 17:49:23 +0800

    finish city-ajax

--- travelapp/src/pages/City/components/Alphabet.vue
+++ travelapp/src/pages/City/components/Alphabet.vue
@@ -1,21 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item">A</li>
-    <li class="item">B</li>
-    <li class="item">C</li>
-    <li class="item">D</li>
-    <li class="item">E</li>
-    <li class="item">F</li>
-    <li class="item">G</li>
-    <li class="item">H</li>
-    <li class="item">J</li>
-    <li class="item">K</li>
+    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
   </ul>
 </template>
 
 <script>
 export default {
-  name: 'CityAplpabet'
+  name: 'CityAplpabet',
+  props: {
+    cities: Object
+  }
 }
 </script>
 
