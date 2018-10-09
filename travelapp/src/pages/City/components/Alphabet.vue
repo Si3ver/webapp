c6d6dfce61ed1a435b805539a1a974fd7e49c6dc
Si3ver <zhou_weilin@outlook.com>
2018-10-09 17:25:25 +0800

    finish city css

---
+++ travelapp/src/pages/City/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">B</li>
+    <li class="item">C</li>
+    <li class="item">D</li>
+    <li class="item">E</li>
+    <li class="item">F</li>
+    <li class="item">G</li>
+    <li class="item">H</li>
+    <li class="item">J</li>
+    <li class="item">K</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAplpabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~@/assets/styles/varibles.styl'
+  .list
+    display: flex
+    flex-direction: column
+    justify-content: center
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
