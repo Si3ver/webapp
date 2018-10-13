a977b7b77416baac84d379bbf18415214ed85372
Si3ver <zhou_weilin@outlook.com>
2018-10-13 09:57:28 +0800

    absolute path

--- travelapp/src/pages/City/City.vue
+++ travelapp/src/pages/City/City.vue
@@ -37,7 +37,7 @@ export default {
   },
   methods: {
     getCityInfo () {
-      axios.get('/api/city.json')
+      axios.get('/webapp/travelapp/dist/static/mock/city.json')
         .then(this.handleGetCityInfoSucc)
     },
     handleGetCityInfoSucc (res) {
