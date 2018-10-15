8570e4ffbfd20122e30368c398a91137b02b024b
Si3ver <zhou_weilin@outlook.com>
2018-10-15 20:57:30 +0800

    add animation for swiper

--- travelapp/src/pages/detail/components/Banner.vue
+++ travelapp/src/pages/detail/components/Banner.vue
@@ -10,16 +10,19 @@
         </div>
       </div>
     </div>
-    <common-gallary
-      :imgs="this.gallaryImgs"
-      v-show="showGallary"
-      @close="handleGallaryClose"
-    ></common-gallary>
+    <fade-animation>
+      <common-gallary
+        :imgs="this.gallaryImgs"
+        v-show="showGallary"
+        @close="handleGallaryClose"
+      ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -42,7 +45,8 @@ export default {
     }
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   }
 }
 </script>
